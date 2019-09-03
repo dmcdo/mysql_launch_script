13eeaa89b755c8c377c5820ef3bbc2c01e60f592
dylanmcdougall <dylanmcdougall@protonmail.com>
2019-09-03 14:09:22 -0400

    Initial Commit

---
+++ mysql.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+ICONPATH="$( cd "$(dirname "$0")" ; pwd -P )/icon.svg"
+
+loop=true
+systemctl start mysql
+
+# If mysql did not start, do not enter loop
+# (It's likely that user cancelled the operation)
+if [[ $(pgrep mysql) == "" ]]; then
+    loop=false
+fi
+
+while $loop
+do
+    # Show tray icon
+    yad --notification --command='quit' --image=$ICONPATH
+    # Hold until toggled, then close tray icon
+
+    # Once tray icon has been toggled
+    systemctl stop mysql
+
+    # If mysql process exited, exit the loop
+    if [[ $(pgrep mysql) == "" ]]; then
+        loop=false
+    else
+        # Otherwise, notify the user
+        notify-send "MySQL" "The MySQL service was not ended" --icon=$ICONPATH
+    fi
+done
